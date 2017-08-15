4a9150cb1d32d7eda637cba8e51b505e7ef44b30
Diane Zheng <dianezheng@Dianes-MBP.dhcp.lbl.gov>
2017-08-15 16:23:27 -0700

    switch to simpler example

--- src/plugin/modules/widgets/provenanceWidget.js
+++ src/plugin/modules/widgets/provenanceWidget.js
@@ -977,156 +977,65 @@ define([
 
             }
             function renderTest(){
-              var w = 1000;
-              var h = 600;
+              var margin = {top: 10, right: 10, bottom: 10, left: 10},
+                  width = config.width - 50 - margin.left - margin.right,
+                  height = graph.nodes.length * 38 - margin.top - margin.bottom
               var linkDistance=200;
+              var nodes = [
+                  { x:   width/3, y: height/2 },
+                  { x: 2*width/3, y: height/2 }
+              ];
 
-              var colors = d3.scale.category10();
+              var links = [
+                  { source: 0, target: 1 }
+              ];
+              var nodes = referenceGraph.nodes
+              //
+              var links = referenceGraph.linkes;
 
-              var dataset = {
-
-              nodes: [
-              {name: "Adam"},
-              {name: "Bob"},
-              {name: "Carrie"},
-              {name: "Donovan"},
-              {name: "Edward"},
-              {name: "Felicity"},
-              {name: "George"},
-              {name: "Hannah"},
-              {name: "Iris"},
-              {name: "Jerry"}
-              ],
-              edges: [
-              {source: 0, target: 1},
-              {source: 0, target: 2},
-              {source: 1, target: 3},
-              {source: 1, target: 4},
-
-              ]
-              };
-
-
-              var svg = d3.select("body").append("svg").attr({"width":w,"height":h});
-
+              var svg = d3.select('body').append('svg')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .style("border", "1px solid black")   // fill the text with the colour black
               var force = d3.layout.force()
-                  .nodes(dataset.nodes)
-                  .links(dataset.edges)
-                  .size([w,h])
-                  .linkDistance([linkDistance])
-                  .charge([-500])
-                  .theta(0.1)
-                  .gravity(0.05)
-                  .start();
+                  .size([width, height])
+                  .nodes(nodes)
+                  .links(links);
+
+              force.linkDistance(width/2);
+
+              var link = svg.selectAll('.link')
+                  .data(links)
+                  .enter().append('line')
+                  .attr('class', 'link')
+                  .style('stroke', 'black')
+                  .style('stroke-width', '3px');
+
+              var node = svg.selectAll('.node')
+                  .data(nodes)
+                  .enter().append('circle')
+                  .attr('class', 'node')
+                  .style('fill', 'pink')
+                  .style('stroke', 'blue')
+                  .style('stroke-width', '2px');
 
 
+              force.on('end', function() {
+                  node.attr('r', width/25)
+                      .attr('cx', function(d) { return d.x; })
+                      .attr('cy', function(d) { return d.y; });
 
-              var edges = svg.selectAll("line")
-                .data(dataset.edges)
-                .enter()
-                .append("line")
-                .attr("id",function(d,i) {return 'edge'+i})
-                .attr('marker-end','url(#arrowhead)')
-                .style("stroke","#ccc")
-                .style("pointer-events", "none");
+                  link.attr('x1', function(d) { return d.source.x; })
+                      .attr('y1', function(d) { return d.source.y; })
+                      .attr('x2', function(d) { return d.target.x; })
+                      .attr('y2', function(d) { return d.target.y; });
 
-              var nodes = svg.selectAll("circle")
-                .data(dataset.nodes)
-                .enter()
-                .append("circle")
-                .attr({"r":15})
-                .style("fill",function(d,i){return colors(i);})
-                .call(force.drag)
-
-
-              var nodelabels = svg.selectAll(".nodelabel")
-                 .data(dataset.nodes)
-                 .enter()
-                 .append("text")
-                 .attr({"x":function(d){return d.x;},
-                        "y":function(d){return d.y;},
-                        "class":"nodelabel",
-                        "stroke":"black"})
-                 .text(function(d){return d.name;});
-
-              var edgepaths = svg.selectAll(".edgepath")
-                  .data(dataset.edges)
-                  .enter()
-                  .append('path')
-                  .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
-                         'class':'edgepath',
-                         'fill-opacity':0,
-                         'stroke-opacity':0,
-                         'fill':'blue',
-                         'stroke':'red',
-                         'id':function(d,i) {return 'edgepath'+i}})
-                  .style("pointer-events", "none");
-
-              var edgelabels = svg.selectAll(".edgelabel")
-                  .data(dataset.edges)
-                  .enter()
-                  .append('text')
-                  .style("pointer-events", "none")
-                  .attr({'class':'edgelabel',
-                         'id':function(d,i){return 'edgelabel'+i},
-                         'dx':80,
-                         'dy':0,
-                         'font-size':10,
-                         'fill':'#aaa'});
-
-              edgelabels.append('textPath')
-                  .attr('xlink:href',function(d,i) {return '#edgepath'+i})
-                  .style("pointer-events", "none")
-                  .text(function(d,i){return 'label '+i});
-
-
-              svg.append('defs').append('marker')
-                  .attr({'id':'arrowhead',
-                         'viewBox':'-0 -5 10 10',
-                         'refX':25,
-                         'refY':0,
-                         //'markerUnits':'strokeWidth',
-                         'orient':'auto',
-                         'markerWidth':10,
-                         'markerHeight':10,
-                         'xoverflow':'visible'})
-                  .append('svg:path')
-                      .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-                      .attr('fill', '#ccc')
-                      .attr('stroke','#ccc');
-
-
-              force.on("tick", function(){
-
-                  edges.attr({"x1": function(d){return d.source.x;},
-                              "y1": function(d){return d.source.y;},
-                              "x2": function(d){return d.target.x;},
-                              "y2": function(d){return d.target.y;}
-                  });
-
-                  nodes.attr({"cx":function(d){return d.x;},
-                              "cy":function(d){return d.y;}
-                  });
-
-                  nodelabels.attr("x", function(d) { return d.x; })
-                            .attr("y", function(d) { return d.y; });
-
-                  edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
-                                                     //console.log(d)
-                                                     return path});
-
-                  edgelabels.attr('transform',function(d,i){
-                      if (d.target.x<d.source.x){
-                          bbox = this.getBBox();
-                          rx = bbox.x+bbox.width/2;
-                          ry = bbox.y+bbox.height/2;
-                          return 'rotate(180 '+rx+' '+ry+')';
-                          }
-                      else {
-                          return 'rotate(0)';
-                          }
-                  });
               });
+
+              // Okay, everything is set up now so it's time to turn
+              // things over to the force layout. Here we go.
+
+              force.start();
             }
 
             function finishUpAndRender() {
